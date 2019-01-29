8d6b50ee71ddcf6c86d5c5b973a846b4993c7a75
Patrik Dvořáček <dvoracek.patrik@gmail.com>
2019-01-29 23:28:44 +0100

    Created Match Component
    Match now serves as the main component of Bracket. Winner is chosen through Radio Buttons.

---
+++ src/Components/Match.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Radio, RadioGroup, FormControlLabel, FormControl } from '@material-ui/core'
+
+class Match extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {value: ""};
+
+        this.handleScoreChange = this.handleScoreChange.bind(this);
+    }
+
+    handleScoreChange(e)
+    {
+        this.setState({value: e.target.value});
+    }
+
+    render() {
+        return (
+            <div>
+                <li className="spacer">&nbsp;</li>
+                <FormControl component="fieldset">
+                    <RadioGroup aria-label="Match" value={this.state.value} onChange={this.handleScoreChange}>
+                        <FormControlLabel value={this.props.topTeam} control={<Radio />} label={this.props.topTeam} />
+                        <span className="game game-spacer">&nbsp;</span>
+                        <FormControlLabel value={this.props.bottomTeam} control={<Radio />} label={this.props.bottomTeam} />
+                    </RadioGroup>
+                </FormControl>
+            </div>
+        );
+    }
+}
+
+export default Match
