b9d566c48a9ac2f528835a08245a9cb7e273ffab
Patrik Dvořáček <dvoracek.patrik@gmail.com>
2019-02-02 19:52:47 +0100

    Updates

---
+++ precache-manifest.6155d1278457e70f8b2855751a396847.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "dda699e3735c51b235e3",
+    "url": "/Tournaments/static/js/main.dda699e3.chunk.js"
+  },
+  {
+    "revision": "b9c451942aeb445d1fb1",
+    "url": "/Tournaments/static/js/1.b9c45194.chunk.js"
+  },
+  {
+    "revision": "23da14b32b9ca505b8ff",
+    "url": "/Tournaments/static/js/runtime~main.23da14b3.js"
+  },
+  {
+    "revision": "f473a15d0d069a53a48edf8a456cbfa9",
+    "url": "/Tournaments/index.html"
+  }
+];
